1cfeec27773118ed1bccea7007a285a8d1670a1d
sameesayeed007 <59398279+sameesayeed007@users.noreply.github.com>
2020-09-01 12:01:54 +0600

    Update admin.py

--- Intense/admin.py
+++ Intense/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from Intense.models import User,Profile,user_balance,user_relation
+from Intense.models import User,Profile,user_balance,user_relation,FAQ
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(user_balance)
 admin.site.register(user_relation)
+admin.site.register(FAQ)
