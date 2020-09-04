106644bcae6f3a29b8cf1c5b9b219e0d01fa48d1
sameesayeed007 <samee880@gmail.com>
2020-09-04 07:25:35 -0700

    Again

--- Tango/settings.py
+++ Tango/settings.py
@@ -133,7 +133,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tester',
+        'NAME': 'test4',
         'USER': 'postgres',
         'PASSWORD': '12345',
         'HOST': 'localhost',
