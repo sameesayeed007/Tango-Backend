c183568961c49c29acafc2c8c2364dc22e483150
sameesayeed007 <samee880@gmail.com>
2020-09-15 22:07:20 -0700

    whitelist added

--- Tango/settings.py
+++ Tango/settings.py
@@ -96,6 +96,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://tes.com.bd:49000',
     'https://front-end-134.herokuapp.com',
+    'https://tangoadmin.herokuapp.com',
 ]
 
 ROOT_URLCONF = 'Tango.urls'
